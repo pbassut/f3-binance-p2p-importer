a609f57eb1587f6c4dab70e724ecaef981e87e31
Patrick Bassut <patrickbassut@hotmail.com>
2025-06-28 18:49:00 -0300

    rule

--- scripts/process-example.ts
+++ scripts/process-example.ts
@@ -1,6 +1,12 @@
 import { processCsvFile } from "../src/process";
 import fs from "fs";
 
-processCsvFile("example.csv", "example.out.csv", () => {
-  console.log(fs.readFileSync("example.out.csv", "utf8"));
+// Get command-line arguments (node scripts/process-example.ts input.csv output.csv)
+const [input, output] = process.argv.slice(2);
+
+const inputFile = input || "example.csv";
+const outputFile = output || "example.out.csv";
+
+processCsvFile(inputFile, outputFile, () => {
+  console.log(fs.readFileSync(outputFile, "utf8"));
 });
